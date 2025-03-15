dd5d08b801613a267138035f166b3bd9d2ce1ac4
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-03-15 08:04:37 +0700

    Web3 provider

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,4 +3,8 @@ const { Web3 } = require('web3');
 
 // updated ganache and web3 imports added for convenience
 
+const assert = require('assert');
+
 // contract test code will go here
+const web3 = new Web3(ganache.provider());
+
