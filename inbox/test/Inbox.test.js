c0217720e7ed5f3f5984b60bbb466bb2ff9eab3f
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-03-15 17:09:39 +0700

    Assert deployment

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,9 +2,13 @@ const ganache = require('ganache');
 const assert = require('assert');
 const { Web3 } = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
 // updated ganache and web3 imports added for convenience
 
 // contract test code will go here
+let accounts;
+let inbox;
 // refactor to async-await patterns 
 
 beforeEach( async () => {
@@ -12,11 +16,16 @@ beforeEach( async () => {
     accounts = await web3.eth.getAccounts();
 
     // Use one of those accounts to deploy contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!']})
+        .send( { from: accounts[0] , gas: '1000000'})
+
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts);
+        // ok method is checking if the value is exist ?
+        assert.ok(inbox.options.address);
     })
 })
 
